efacbad2854b1980cd64aed2cdd6a6a3e5117308
phamlong56 <mrphamlong56@gmail.com>
2024-09-21 11:07:25 +0700

    thuc hanh: find min array

---
+++ 3-Array/find-min-array/src/NumberMinArray.java
@@ -0,0 +1,21 @@
+public class NumberMinArray {
+
+    public static void main(String[] args) {
+        int []arr ={4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest number is " + arr[index]);
+    }
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+
+            }
+            System.out.println(array[index]);
+        }
+        return index;
+    }
+
+
+}
