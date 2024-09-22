4c11c47ac10be07c1ef320a2c134f257ba3a4046
phamlong56 <mrphamlong56@gmail.com>
2024-09-22 07:28:28 +0700

    thuc hanh: ke thua shape

---
+++ 6-Inheritance/shape/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+        shape = new Shape("red", false);
+        System.out.println(shape);
+        Shape shape2 = new Shape("black", false);
+        System.out.println(shape2);
+
+        Circle circle = new Circle();
+        System.out.println(circle);
+        circle = new Circle(4);
+        System.out.println(circle);
+        circle = new Circle("pink",true,2);
+        System.out.println(circle);
+
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+        rectangle = new Rectangle(4,5);
+        System.out.println(rectangle);
+        rectangle = new Rectangle("red",false,2,3);
+        System.out.println(rectangle);
+
+    }
+}
